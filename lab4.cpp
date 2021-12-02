865fb7f304e7bdbb263c5425bc363687ee70a97c
Michał Roczek <72460333+roczekmichal@users.noreply.github.com>
2021-12-02 20:47:25 +0100

    4th labs

--- lab4.cpp
+++ lab4.cpp
@@ -56,69 +56,6 @@
 
 using namespace std;
 
-// *******************************************************
-// Functions used in exercise 3
-int N = 1000000;
-// Returns random number
-int random_num()
-{
-    static std::default_random_engine gen(std::random_device{}());
-    static std::uniform_int_distribution<int> dist;
-    return dist(gen, std::uniform_int_distribution<int>::param_type{});
-}
-// Returns dynamically allocated array of integers, filled with random numbers
-int * random_num_tab(int n)
-{
-    int * tab = new int[n];
-    for (int i = 0; i < n; i++)
-        tab[i] = random_num();
-    return tab;
-}
-// Measures and returns time of execution of func(tab,n)
-double measure_time(void(*func)(int *, int), int * tab, int n)
-{
-    auto start = std::chrono::high_resolution_clock::now();
-    func(tab, n);
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> duration = end - start;
-    return duration.count();
-}
-// Measures and returns time of 3 executions of func(tab,n)
-double measure_time_avg3(void(*func)(int *, int), int * tab, int n)
-{
-    double c;
-    c = measure_time(func, tab, n);
-    c += measure_time(func, tab, n);
-    c += measure_time(func, tab, n);
-    return c/3;
-}
-
-void vector_fill1(int * data, int n)
-{
-    vector<int> v;
-    for (int i = 0; i < n; i++)
-    {
-        v.push_back(data[i]);
-    }
-}
-
-void vector_fill2(int * data, int n)
-{
-    // ...
-}
-
-void vector_fill3(int * data, int n)
-{
-    // ...
-}
-
-//...
-
-
-// *******************************************************
-
-
-// *******************************************************
 // Functions used in exercise 4
 template <class T>
 double measure_time(int(*func)(const T&),const  T &t)
@@ -153,8 +90,6 @@ int vector_max(const vector<int>& v)
 // ...
 
 
-// *******************************************************
-
 int main() {
     
     //************* EX 1 *************//
@@ -173,17 +108,35 @@ int main() {
     //  - unordered_map
     //  - unordered_multimap
     // In case of *map containers (last four containers) use number from num array as key and its index in num array as value
-    
-    
+     
     vector<int> v(num, num+num_size);
     set<int> s(num, num+num_size);
     multiset<int> ms(num, num+num_size);
     unordered_set<int> us(num,num+num_size);
     unordered_multiset<int> ums(num,num+num_size);
+
     map<int,int> m;
     for(int i = 0; i < num_size; i++)
     {
-        m.insert(std::pair<int,int>(num[i],i));
+        m.insert({num[i],i});
+    }
+
+    multimap<int,int> mm;
+    for(int i = 0; i < num_size; i++)
+    {
+        mm.insert({num[i],i});
+    }
+
+    unordered_map<int,int> um;
+    for(int i = 0; i < num_size; i++)
+    {
+        um.insert({num[i],i});
+    }
+
+    unordered_multimap<int, int> umm;
+    for(int i = 0; i < num_size; i++)
+    {
+        umm.insert({num[i],i});
     }
     // 1.b) Display all elements from each container
     cout << endl << "Content of vector:"<< endl;
@@ -212,13 +165,25 @@ int main() {
         cout << i << " ";
     }
     cout << endl << "Content of map:"<< endl;
-
+    for( auto & el : m)
+    {
+       cout << el.first << ":" << el.second << " ";
+    }
     cout << endl << "Content of multimap:"<< endl;
-    // ...
+    for( auto & el : mm)
+    {
+       cout << el.first << ":" << el.second << " ";
+    }
     cout << endl << "Content of unordered_map:"<< endl;
-    // ...
+    for( auto & el : um)
+    {
+       cout << el.first << ":" << el.second << " ";
+    }    
     cout << endl << "Content of unordered_multimap:"<< endl;
-    // ...
+    for( auto & el : umm)
+    {
+       cout << el.first << ":" << el.second << " ";
+    }    
     
     
     //************* EX 2 *************//
@@ -230,21 +195,19 @@ int main() {
     map<string, string> countries;
     
     // 2.b) Insert 3 key-value elements into the map using insert method (including {"Poland", "Warsaw"} pair)
-    countries.insert("Poland","Warsaw");
-    countries.insert("England","London");
-    countries.insert("France","Paris");
+    countries.insert({"Poland","Warsaw"});
+    countries.insert({"England","London"});
+    countries.insert({"France","Paris"});
     // 2.c) Insert 3 key-value elements into the map using operator[]
-    // ...
     countries["Spain"] = "Madrid";
     countries["Italy"] = "Rome";
     countries["Germany"] = "Berlin";
     // 2.d) Display all elements of the map (country-capital pairs)
     cout << "Country-Capital map:"<< endl;
-    // ...
-   for( auto & el : countries)
-   {
+    for( auto & el : countries)
+    {
        cout << el.first << ":" << el.second << " ";
-   }
+    } 
     // 2.e) Change the capital of Poland to "Cracow".
     countries["Poland"] = "Cracow";
     
@@ -253,65 +216,17 @@ int main() {
     
     // 2.g) Display all elements of the map again (country-capital pairs).
     cout << endl << "Country-Capital map after changes:"<< endl;
-    // ...
+    for( auto & el : countries)
+    {
+        cout << el.first << ":" << el.second << " ";
+    }
     // 2.h) Construct another map using range constructor and copying elements of the country-capital map up to "Poland" (country-capital pairs). Display it.
-    // ...
+    map<string, string> another_countries_map (countries.begin(), countries.find("Poland"));
     cout << endl << "Another Country-Capital map:"<< endl;
-    // ...
-    
-    //************* EX 3 *************//
-    cout << endl << endl << "************* EX 3 *************" << endl;
-    int n = N;
-    int * data = random_num_tab(n); //data: dynamically allocated array of integers
-   // 3. Measure time of initializing containers and filling it with numbers from data array. Containers that should be used for test (uncomment code and define appropriate functions):
-    // -vector (using push_back()) [DONE]
-    // -vector (with reserve(N) used before push_back())
-    // -vector (with initial size N and operator[] used)
-    // -vector (with range constructor used)
-    // -list
-    // -stack
-    // -forward_list
-    // -deque
-    // -queue
-    // -priority_queue
-    // -dynamically allocated array
-    // -set
-    // -multiset
-    // -map
-    // -multimap
-    // -unordered_set
-    // -unordered_multiset
-    // -unordered_map
-    // For all *map containers use char as the value and set character 'a' for all elements
-    
-    cout.precision(6);
-    cout << fixed;
-    cout << "-------------------" << endl;
-    cout << "Data size: " <<  n << endl;
-    cout << "Initialization and filling with numbers"<< endl;
-    cout << "-------------------" << endl;
-    cout << "Time(s)\t\tContainer" << endl;
-    cout << "-------------------" << endl;
-    cout << measure_time_avg3(vector_fill1, data, n) <<  "\tvector" << endl;
-    cout << measure_time_avg3(vector_fill2, data, n) <<  "\tvector + reserve" <<endl;
-    cout << measure_time_avg3(vector_fill3, data, n) <<  "\tvector with initial size n + []" << endl;
-    //cout << measure_time_avg3(vector_fill4, data, n) <<  "\tvector with range constructor" << endl;
-    //cout << measure_time_avg3(lista_fill, data, n) <<  "\tlist" << endl;
-    //cout << measure_time_avg3(stack_fill, data, n) <<  "\tstack" << endl;
-    //cout << measure_time_avg3(forward_lista_fill, data, n) <<  "\tforward_list" << endl;
-    //cout << measure_time_avg3(deque_fill, data, n) <<  "\tdeque" << endl;
-    //cout << measure_time_avg3(queue_fill, data, n) <<  "\tqueue" << endl;
-    //cout << measure_time_avg3(p_queue_fill, data, n) <<  "\tpriority_queue" << endl;
-    //cout << measure_time_avg3(tab_fill, data, n) <<  "\tdynamically allocated array" <<endl;
-    cout << "--Associative containers--" << endl;
-    //cout << measure_time_avg3(set_fill, data, n) <<  "\tset" <<endl;
-    //cout << measure_time_avg3(multiset_fill, data, n) <<  "\tmultiset" <<endl;
-    //cout << measure_time_avg3(map_fill, data, n) <<  "\tmap" <<endl;
-    //cout << measure_time_avg3(multimap_fill, data, n) <<  "\tmultimap" <<endl;
-    //cout << measure_time_avg3(unordered_set_fill, data, n) <<  "\tunordered_set" <<endl;
-    //cout << measure_time_avg3(unordered_multiset_fill, data, n) <<  "\tunordered_multiset" <<endl;
-    //cout << measure_time_avg3(unordered_map_fill, data, n) <<  "\tunordered_map" <<endl;
-    //cout << measure_time_avg3(unordered_multimap_fill, data, n) <<  "\tunordered_multimap" <<endl;
+    for( auto & el : another_countries_map)
+    {
+        cout << el.first << ":" << el.second << " ";
+    }
     
 
     //************* EX 4 *************//
@@ -319,10 +234,10 @@ int main() {
     cout.precision(10);
     cout << fixed;
    // 4.a) Use numbers in data to create:
-    // - vector v
-    // - priority_queue q
-    // - set s
-    // - unordered_set us
+    // - vector v1
+    // - priority_queue q1
+    // - set s1
+    // - unordered_set us1
     
     //...
     
@@ -330,7 +245,7 @@ int main() {
     // (as in 3., complete sunctions)
     cout <<endl << endl;
     cout << "-------------------" << endl;
-    cout << "Data size: " <<  n << endl;
+    //cout << "Data size: " <<  n << endl;
     cout << "Finding max"<< endl;
     cout << "-------------------" << endl;
     cout << "Time(s)\tStructure" << endl;
@@ -346,7 +261,7 @@ int main() {
     // 4.c) Measure time of finding number "-1" in vector, set, unordered_set
     
     cout << "-------------------" << endl;
-    cout << "Data size: " <<  n << endl;
+    //cout << "Data size: " <<  n << endl;
     cout << "Finding element"<< endl;
     cout << "-------------------" << endl;
     cout << "Tiome(s)\tStruscture" << endl;
@@ -355,12 +270,6 @@ int main() {
  //   cout << measure_time_avg3(set_find, s) <<  "\tset" << endl;
  //   cout << measure_time_avg3(u_set_find, us) <<  "\tunordered_set" << endl;
     cout << "-------------------" << endl;
-  
-    
-
-    
-    
-    
     
 }
 
